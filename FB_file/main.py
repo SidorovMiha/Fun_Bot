0d72afd4520c714895c13f438c0412509dfd1086
SidorovMiha <90196468+SidorovMiha@users.noreply.github.com>
2021-12-17 21:16:21 +0300

    Delete main.py

--- FB_file/main.py
+++
@@ -1,7 +0,0 @@
-from server import Server
-from config import vk_api_token, group_id, response_id
-from menu import Menu
-
-server1 = Server(vk_api_token, group_id, response_id, "server1")
-
-server1.start()
